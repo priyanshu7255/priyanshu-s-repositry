ab11067a817d20253005bd5550afdc40b5e7b01e
Priyanshu7255 <priyanshu23242526@gmail.com>
2024-07-04 12:21:50 +0530

    oddnumberevernumber

---
+++ oddevennum.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+
+{
+ int number;
+
+ prinf("please enter any number :");
+ scanf("%d",&number);
+
+ if (number % 2 ==0)
+ {
+    prinf("%d is even.\n",number);
+ }
+
+ else{
+
+    printf("%d is odd.\n",number);
+ }
+
+
+
+
+
+
+
+
+
+    return 0;
+}
